bd3b0ad86675837e6fc72a5f774706af83b51d0c
antonezin <72981447+antonezin@users.noreply.github.com>
2021-04-15 19:22:10 +0700

    Update footer.php

--- app/footer.php
+++ app/footer.php
@@ -5,7 +5,7 @@
 
 </div>
 
-		<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+		<script src="../public/js/jquery-3.5.1.js"></script>
 		
 		<script src="../public/js/main.js"></script>
 		
